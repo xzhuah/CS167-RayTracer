d9b56e7b4de22b3ca5264a48cd6edb9754670853
ZHU Xinyu <xzhuah@connect.ust.hk>
2017-03-19 18:25:28 -0700

    add inverse function

--- HW4/HW4/matrix44.h
+++ HW4/HW4/matrix44.h
@@ -7,6 +7,7 @@ public:
     matrix44();
     matrix44(const float);
 	matrix44(const float[4][4]);
+	matrix44(const matrix44&&);
 
 	//static matrix44 createMatrix44(float rotateX, float rotateY, float rotateZ, float tran_x, float tran_y, float tran_z, float scale_x, float scale_y, float scale_z);
 	static matrix44 createScaleMatrix44(float scale_x, float scale_y, float scale_z);
@@ -19,9 +20,11 @@ public:
     matrix44 operator+(const matrix44&) const;
     matrix44 operator*(const float&) const;
 	matrix44 operator*(const matrix44&) const;
+	matrix44& operator=(const matrix44&);
 
 	static matrix44 inverse(const matrix44&);
 	static matrix44 transpose(const matrix44&);
+	static bool gluInvertMatrix(const float m[16], float invOut[16]);
 };
 matrix44::matrix44() {
     for (int i = 0; i < 4; i++)
@@ -38,6 +41,13 @@ matrix44::matrix44(const float the_matrix[4][4]) {
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
 			this->matrix[i][j] = the_matrix[i][j];
+		}
+	}
+};
+matrix44::matrix44(const matrix44&& mat) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			this->matrix[i][j] = mat.matrix[i][j];
 		}
 	}
 };
@@ -135,10 +145,15 @@ matrix44 matrix44::operator*(const matrix44& other) const {
 	matrix44 final(result);
 	return final;
 }
+matrix44& matrix44::operator=(const matrix44& mat) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			this->matrix[i][j] = mat.matrix[i][j];
+		}
+	}
+	return *this;
+};
 
-matrix44 matrix44::inverse(const matrix44& mat) {
-    
-}
 matrix44 matrix44::transpose(const matrix44& mat) {
 	float result[4][4];
 	for (int i = 0; i < 4; i++) {
@@ -148,4 +163,167 @@ matrix44 matrix44::transpose(const matrix44& mat) {
 	}
 	matrix44 final(result);
 	return final;
+}
+
+matrix44 matrix44::inverse(const matrix44& the_mat) {
+	float m[16];
+	float out[16];
+	int row = 0;
+	int col = 0;
+	for (int i = 0; i < 16; i++) {
+		m[i] = the_mat.matrix[row][col];
+		row++;
+		if (row > 3) {
+			row = 0;
+			col++;
+		}
+	}
+	matrix44 result;
+	if (gluInvertMatrix(m, out)) {
+		int row = 0;
+		int col = 0;
+		for (int i = 0; i < 16; i++) {
+			result.matrix[row][col] = out[i];
+			row++;
+			if (row > 3) {
+				row = 0;
+				col++;
+			}
+		}
+		return result;
+	}
+	else {
+
+		return result;
+	}
+
+}
+bool matrix44::gluInvertMatrix(const float m[16], float invOut[16])
+{
+	float inv[16], det;
+	int i;
+
+	inv[0] = m[5] * m[10] * m[15] -
+		m[5] * m[11] * m[14] -
+		m[9] * m[6] * m[15] +
+		m[9] * m[7] * m[14] +
+		m[13] * m[6] * m[11] -
+		m[13] * m[7] * m[10];
+
+	inv[4] = -m[4] * m[10] * m[15] +
+		m[4] * m[11] * m[14] +
+		m[8] * m[6] * m[15] -
+		m[8] * m[7] * m[14] -
+		m[12] * m[6] * m[11] +
+		m[12] * m[7] * m[10];
+
+	inv[8] = m[4] * m[9] * m[15] -
+		m[4] * m[11] * m[13] -
+		m[8] * m[5] * m[15] +
+		m[8] * m[7] * m[13] +
+		m[12] * m[5] * m[11] -
+		m[12] * m[7] * m[9];
+
+	inv[12] = -m[4] * m[9] * m[14] +
+		m[4] * m[10] * m[13] +
+		m[8] * m[5] * m[14] -
+		m[8] * m[6] * m[13] -
+		m[12] * m[5] * m[10] +
+		m[12] * m[6] * m[9];
+
+	inv[1] = -m[1] * m[10] * m[15] +
+		m[1] * m[11] * m[14] +
+		m[9] * m[2] * m[15] -
+		m[9] * m[3] * m[14] -
+		m[13] * m[2] * m[11] +
+		m[13] * m[3] * m[10];
+
+	inv[5] = m[0] * m[10] * m[15] -
+		m[0] * m[11] * m[14] -
+		m[8] * m[2] * m[15] +
+		m[8] * m[3] * m[14] +
+		m[12] * m[2] * m[11] -
+		m[12] * m[3] * m[10];
+
+	inv[9] = -m[0] * m[9] * m[15] +
+		m[0] * m[11] * m[13] +
+		m[8] * m[1] * m[15] -
+		m[8] * m[3] * m[13] -
+		m[12] * m[1] * m[11] +
+		m[12] * m[3] * m[9];
+
+	inv[13] = m[0] * m[9] * m[14] -
+		m[0] * m[10] * m[13] -
+		m[8] * m[1] * m[14] +
+		m[8] * m[2] * m[13] +
+		m[12] * m[1] * m[10] -
+		m[12] * m[2] * m[9];
+
+	inv[2] = m[1] * m[6] * m[15] -
+		m[1] * m[7] * m[14] -
+		m[5] * m[2] * m[15] +
+		m[5] * m[3] * m[14] +
+		m[13] * m[2] * m[7] -
+		m[13] * m[3] * m[6];
+
+	inv[6] = -m[0] * m[6] * m[15] +
+		m[0] * m[7] * m[14] +
+		m[4] * m[2] * m[15] -
+		m[4] * m[3] * m[14] -
+		m[12] * m[2] * m[7] +
+		m[12] * m[3] * m[6];
+
+	inv[10] = m[0] * m[5] * m[15] -
+		m[0] * m[7] * m[13] -
+		m[4] * m[1] * m[15] +
+		m[4] * m[3] * m[13] +
+		m[12] * m[1] * m[7] -
+		m[12] * m[3] * m[5];
+
+	inv[14] = -m[0] * m[5] * m[14] +
+		m[0] * m[6] * m[13] +
+		m[4] * m[1] * m[14] -
+		m[4] * m[2] * m[13] -
+		m[12] * m[1] * m[6] +
+		m[12] * m[2] * m[5];
+
+	inv[3] = -m[1] * m[6] * m[11] +
+		m[1] * m[7] * m[10] +
+		m[5] * m[2] * m[11] -
+		m[5] * m[3] * m[10] -
+		m[9] * m[2] * m[7] +
+		m[9] * m[3] * m[6];
+
+	inv[7] = m[0] * m[6] * m[11] -
+		m[0] * m[7] * m[10] -
+		m[4] * m[2] * m[11] +
+		m[4] * m[3] * m[10] +
+		m[8] * m[2] * m[7] -
+		m[8] * m[3] * m[6];
+
+	inv[11] = -m[0] * m[5] * m[11] +
+		m[0] * m[7] * m[9] +
+		m[4] * m[1] * m[11] -
+		m[4] * m[3] * m[9] -
+		m[8] * m[1] * m[7] +
+		m[8] * m[3] * m[5];
+
+	inv[15] = m[0] * m[5] * m[10] -
+		m[0] * m[6] * m[9] -
+		m[4] * m[1] * m[10] +
+		m[4] * m[2] * m[9] +
+		m[8] * m[1] * m[6] -
+		m[8] * m[2] * m[5];
+
+	det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+	if (det == 0)
+		return false;
+
+	det = 1.0 / det;
+
+	for (i = 0; i < 16; i++)
+		invOut[i] = inv[i] * det;
+
+	return true;
 }
