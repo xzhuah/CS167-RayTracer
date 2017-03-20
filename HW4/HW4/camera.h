3de6140348ad3d889678e140522d975ca2b5cf8d
ZHU Xinyu <xzhuah@connect.ust.hk>
2017-03-19 23:51:06 -0700

    fix camera

--- HW4/HW4/camera.h
+++ HW4/HW4/camera.h
@@ -23,7 +23,7 @@ public:
 		u = u.cross(w);
 		u.normalize();
 		camera_right = u;
-		camera_up = w.cross(u);
+		camera_up = w.cross(u).normalize();
 		
 		this->fovy = fovy;
 	}
@@ -32,7 +32,7 @@ public:
 		point source(this->posi);
 		float alpha = tan(fovy / 2)*(j - width / 2) / (width / 2);
 		float blta= tan(fovy / 2)*(i-height/2) / (height / 2);
-		Vec3 direction_ray = this->camera_front + this->camera_right*alpha + this->camera_up*blta;
+		Vec3 direction_ray = this->camera_front + this->camera_right*(alpha) + this->camera_up*(blta);
 		direction_ray.normalize();
 		return ray(source,direction_ray);
 	}
